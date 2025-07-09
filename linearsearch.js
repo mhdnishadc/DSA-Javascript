174ea0c735393b5c1549e19544ec2ed51ea06290
Muhammed Nishad C <122031511+mhdnishadc@users.noreply.github.com>
2025-07-09 20:29:51 +0530

    Update linearsearch.js

--- linearsearch.js
+++ linearsearch.js
@@ -8,8 +8,8 @@ function linearSearch(arr, n) {
 }
 
 // Input array and target
-const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const target = 12;
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const target = 15;
 
 console.time("linearSearch");
 console.log(linearSearch(inputArray, target));  
