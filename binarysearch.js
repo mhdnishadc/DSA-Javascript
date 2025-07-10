55f33c7cfcef5451dc6cb327c6611b730245e2d2
Muhammed Nishad C <122031511+mhdnishadc@users.noreply.github.com>
2025-07-10 15:17:48 +0530

    Update binarysearch.js

--- binarysearch.js
+++ binarysearch.js
@@ -1,1 +1,32 @@
 
+function binarySearch(arr, targetVal) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === targetVal) {
+            return mid;
+        }
+
+        if (arr[mid] < targetVal) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+const myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+const myTarget = 15;
+
+const result = binarySearch(myArray, myTarget);
+
+if (result !== -1) {
+    console.log("Value", myTarget, "found at index", result);
+} else {
+    console.log("Target not found in array.");
+}
